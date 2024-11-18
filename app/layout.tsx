2daca05a94078403bacc2a0607ee8fb11f1c3ed7
Saim Ahmed <saimahmedqazi@gmail.com>
2024-11-19 03:01:33 +0500

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "./components/Navbar";
-import Contact from "./components/Contact";
+import Contact from "./components/Footer";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
